d4227d4dc6d6ca1680f3b01bff3b4a19fd17b206
Dewei Liu <deweiliu@hotmail.com>
2020-03-01 00:03:16 +0000

    add the framework for preprocessing

--- src/main.m
+++ src/main.m
@@ -9,3 +9,9 @@ addpath("./image_aquisition");
 pedestrians=loadimages("../dataset/pedestrian/");
 positives=loadimages("../dataset/images/pos/");
 negatives=loadimages("../dataset/images/neg/");
+
+%% Pre-processing TODO
+addpath("./preprocessing");
+pedestrians.images=preprocessing(pedestrians.images);
+positives.images=preprocessing(positives.images);
+negatives.images=preprocessing(negatives.images);
---
+++ src/preprocessing/preprocessing.m
@@ -0,0 +1,4 @@
+function preprocessed=preprocessing(images)
+% TODO: pre-process the images
+preprocessed=images;
+end
