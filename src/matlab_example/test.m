5911197c1b702754f0e8e64395b782d9983b6a5d
Dewei Liu <deweiliu@hotmail.com>
2020-03-01 17:52:25 +0000

    add matlab example script

---
+++ src/matlab_example/test.m
@@ -0,0 +1,20 @@
+
+addpath("../image_aquisition");
+pedestrians = loadimages("../../dataset/pedestrian/");
+
+videoFile="../output/video.mp4";
+mkdir("../output");
+output =VideoWriter(convertStringsToChars(videoFile),'MPEG-4');
+output.FrameRate=10;
+open(output);
+for i=1:pedestrians.number
+    img=pedestrians.images(:,:,:,i);
+    
+    writeVideo(output,img);
+end
+
+close(output);
+
+
+PedestrianTrackingFromMovingCameraExample(videoFile,pedestrians);
+    
