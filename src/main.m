962e30cc292cd58d33d387f6cf45433a24e4da95
Dongming Li <40215999@eeecs.qub.ac.uk>
2020-03-02 14:10:51 +0000

    commit message xxxx

--- src/main.m
+++ src/main.m
@@ -33,6 +33,7 @@ addpath("./classification");
 model = train(positives, negatives);
 pedestrians.result = test(pedestrians, model);
 
+
 %% presenting the result TODO
 addpath("./presentation");
 present(pedestrians);
