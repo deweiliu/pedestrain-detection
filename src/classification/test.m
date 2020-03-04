cf65dedd6f55aae99f906e7bf842b6e49cfd420a
Dewei Liu <deweiliu@hotmail.com>
2020-03-04 21:46:34 +0000

    add description comment in feature and classifer

--- src/classification/test.m
+++ src/classification/test.m
@@ -1,3 +1,9 @@
 function result=test(pedestrians, model)
     result = knntest(pedestrians, model, 1);
+    
+        
+    % To feed the feature values of each sliding window to the classifer
+    % featureValues=pedestrians.slidingWindows(scaleIndex).slidingWindows(rowIndex,columnIndex,frameIndex).feature;    
+    % true or false =classifier(featureValues,model);
+    % pedestrians.slidingWindows(scaleIndex).slidingWindows(rowIndex,columnIndex,frameIndex).isHuman= true or false;
 end
--- src/features/featureextraction.m
+++ src/features/featureextraction.m
@@ -2,4 +2,10 @@ function features = featureextraction(object)
     % Placeholder random numbers between 1 -> 10000
     features = [];
     features = [features,randi(10000, object.number, 1)];
+    
+    % To apply feature extraction on each sliding windows
+    % img=pedestrians.slidingWindows(scaleIndex).slidingWindows(rowIndex,columnIndex,frameIndex).image;
+    % imshow(img);
+    % featureValues=featureExtractionFunction(img);
+    % pedestrians.slidingWindows(scaleIndex).slidingWindows(rowIndex,columnIndex,frameIndex).feature=featureValues;
 end
