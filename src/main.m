8bfe400c7cb54cb38ee776446bee9cb92d326210
Dewei Liu <deweiliu@hotmail.com>
2020-03-10 23:26:35 +0000

    format main script

--- src/main.m
+++ src/main.m
@@ -6,7 +6,7 @@ close all
 
 %% Load the scripts from subfolders
 % reference https://uk.mathworks.com/matlabcentral/answers/247180-how-may-i-add-all-subfolders-in-my-matlab-path
-folder = fileparts(which(mfilename)); 
+folder = fileparts(which(mfilename));
 addpath(genpath(folder));
 
 %% Images Aquisition
@@ -15,7 +15,7 @@ positives = loadimages("../dataset/images/pos/");
 negatives = loadimages("../dataset/images/neg/");
 positives.labels = ones(positives.number, 1);
 negatives.labels = zeros(negatives.number, 1);
-pedestrians.labels = zeros(pedestrians.number, 1) -1;
+pedestrians.labels = zeros(pedestrians.number, 1) - 1;
 
 %% Pre-processing TODO
 pedestrians.images = preprocessing(pedestrians);
@@ -36,7 +36,7 @@ knnTest(knnModel);
 %% Sliding windows
 SLIDING_WIDTH = positives.nColumns;
 SLIDING_HEIGHT = positives.nRows;
-SLIDING_SCALES = [0.8, 1.2,1.6, 2];
+SLIDING_SCALES = [0.8, 1.2, 1.6, 2];
 SLIDING_GAP_PERCENTAGE = 0.4; % 40 per cent
 pedestrians.sliding = slidingwindows(pedestrians, SLIDING_WIDTH, SLIDING_HEIGHT, SLIDING_SCALES, SLIDING_GAP_PERCENTAGE);
 
@@ -48,15 +48,14 @@ pedestrians = pedestriansPredictor(pedestrians, svmModel, knnModel);
 
 %% Visualize human images
 frame_index = 20; % It can be 1 to 100 corresponding which frame to visualize
-%visualizePrediction(pedestrians, frame_index)
+visualizePrediction(pedestrians, frame_index)
 
 %% Extract information of pedestrians
-THRESHOLD=2; % Greater or equal to 1, integer
-LABELLING_CONNECTIVITY=4; % 8 or 4  # See https://uk.mathworks.com/help/images/ref/bwlabel.html
-pedestrians.results=informationExtraction(pedestrians,THRESHOLD,LABELLING_CONNECTIVITY);
-
+THRESHOLD = 2; % Greater or equal to 1, integer
+LABELLING_CONNECTIVITY = 4; % 8 or 4  # See https://uk.mathworks.com/help/images/ref/bwlabel.html
+pedestrians.results = informationExtraction(pedestrians, THRESHOLD, LABELLING_CONNECTIVITY);
 
 %% presenting the result
 % Re-run this block to rewatch the result %
-FRAME_PER_SECOND=5;
-presentation(pedestrians,FRAME_PER_SECOND);
+FRAME_PER_SECOND = 5;
+presentation(pedestrians, FRAME_PER_SECOND);
