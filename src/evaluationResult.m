0b2aea6513ea981d5c285377a7b966370dfb9bb6
Dewei Liu <deweiliu@hotmail.com>
2020-03-27 18:19:02 +0000

    add try block

--- src/evaluationResult.m
+++ src/evaluationResult.m
@@ -12,9 +12,15 @@ output = "../output";
 makedir(output);
 
 %% Load result
-load("result.mat");
+try
+    load("result.mat");
+catch
+    % https://uk.mathworks.com/help/matlab/ref/try.html
+    fprintf("Unable to load result.mat. Please make sure you have executed main.m\n");
+    return
+end
 
-% Present the result
+%% Present the result
 FRAME_PER_SECOND = 10;
 
 evaluation(result, FRAME_PER_SECOND);
